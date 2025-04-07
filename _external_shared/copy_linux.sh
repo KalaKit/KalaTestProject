ac1fee0491e897a82d7220426b517b7face5fc93
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-04-08 00:31:35 +0300

    compilation improvements

--- _external_shared/copy_linux.sh
+++ _external_shared/copy_linux.sh
@@ -8,10 +8,10 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Origin paths
 
 CRASH_RELEASE="${ROOT_DIR}/KalaCrashHandler/release/libKalaCrashHandler.so"
-CRASH_DEBUG="${ROOT_DIR}/KalaCrashHandler/release/libKalaCrashHandlerD.so"
+CRASH_DEBUG="${ROOT_DIR}/KalaCrashHandler/debug/libKalaCrashHandlerD.so"
 
-PHYSICS_RELEASE="${ROOT_DIR}/KalaPhysics/release/libKalaPhysics.so"
-PHYSICS_DEBUG="${ROOT_DIR}/KalaPhysics/release/libKalaPhysicsD.so"
+WINDOW_RELEASE="${ROOT_DIR}/KalaWindow/release/libKalaWindow.so"
+WINDOW_DEBUG="${ROOT_DIR}/KalaWindow/debug/libKalaWindowD.so"
 
 # Target paths
 
@@ -21,12 +21,12 @@ DEBUG_DLLS="${ROOT_DIR}/../files/external dlls/debug"
 # Copy release dlls
 
 cp -f "$CRASH_RELEASE" "$RELEASE_DLLS"
-cp -f "$PHYSICS_RELEASE" "$RELEASE_DLLS"
+cp -f "$WINDOW_RELEASE" "$RELEASE_DLLS"
 
 # Copy target dlls
 
 cp -f "$CRASH_DEBUG" "$DEBUG_DLLS"
-cp -f "$PHYSICS_DEBUG" "$DEBUG_DLLS"
+cp -f "$WINDOW_DEBUG" "$DEBUG_DLLS"
 
 echo ""
 echo "Finished copying DLLs!"
